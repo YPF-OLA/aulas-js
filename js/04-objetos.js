96394295a1e19f39b37bb82ca59fbf82e825cd16
YPF-OLA <ygor.pfaria135@gmail.com>
2024-10-02 11:36:08 -0300

    Donwoald

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -20,4 +20,59 @@ const livro = {
         "As duas Torres",
         "O Retorno do Rei"
     ]
+};
+
+//   Exibindo a estrutura
+console.log(livro);
+
+
+// Acesso usando o nome do prop. e índice do array desta prop
+console.log(livro.volume [1]);
+
+// Exemplo 3: array de objetos 
+const livros = [
+    {
+        titulo: "Harry Potter",
+        autor: "J.K. Rowling"
+    },
+    {
+        titulo: "Senhor dos Aneis",
+        autor: "J.R.R. Tolkien"
+    },
+    {
+        titulo: "Ghost Rider",
+        autor: "Neil Peart"
+    },
+   
+];
+
+console.log(livros [1].autor);
+
+// Exercicios:
+
+const aluno = {
+    nome: "Ygor PereiraDe Faria",
+    nascimento: "19/07/2006",
+    telefone:["25455521"],
+    endereco: {
+        rua: "Barnabé Alcantara",
+        numero: "98",
+        bairro: "Parque Boturussu"
+    }    
 }
+
+
+console.log(aluno.nome);
+    
+console.log(aluno.telefone[0]);
+
+console.log(aluno.endereco.bairro);
+
+
+
+
+
+
+
+
+
